4d97eac1b3c1e90a075443b9fb88d02400973039
Hiren M <mistryhiren108@gmail.com>
2026-05-07 17:26:43 +0530

    Add basic API integration (callback based)

--- iOS Concurrency Performance Demo/Core/Networking/APIClient.swift
+++ iOS Concurrency Performance Demo/Core/Networking/APIClient.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+class APIClient{
+    
+    func fetchArticles(completion: @escaping ([Article]) -> Void) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            let decoded = try? JSONDecoder().decode([PostDTO].self, from: data)
+            
+            let article = decoded?.map{
+                Article(title: $0.title, description: $0.body)
+            } ?? []
+            
+            DispatchQueue.main.async {
+                completion(article)
+            }
+        }.resume()
+    }
+    
+}
+
+
+struct PostDTO: Decodable {
+    let title: String
+    let body: String
+}
